e62586a44917ca333740a0472edcbc298d2d5420
petrovm <petrovm@gmail.com>
2018-10-19 17:17:12 +0900

    'L03-03'

--- L03-03.js
+++ L03-03.js
@@ -1,3 +1,22 @@
-function f(a,b,c,d) {
-	return Math.min(Math.max(a,b),Math.max(c,d));
+function f(a,b,c,d)
+	{
+		var max1,max2,min;
+		if (a>b) {
+			max1=a
+		}
+			else {
+			max1=b
+		}
+		if (c>d) {
+			max2=c
+		}
+			else {
+			max2=d
+		}
+		if (max1>max2) {
+			min=max2
+		}
+			else {
+				min=max1
+			}return min;
 	}
