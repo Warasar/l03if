e5ab3bd49f4eddca30241a53857aa42b190370c5
Ambros52 <43155893+Ambros52@users.noreply.github.com>
2018-10-22 23:46:45 +0900

    dindindon

--- L03-13.js
+++ L03-13.js
@@ -3,6 +3,9 @@ function S(x1,y1,x2,y2,x3,y3) {
 	a = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
 	b = Math.sqrt((x2-x3)*(x2-x3)+(y2-y3)*(y2-y3));
 	c = Math.sqrt((x1-x3)*(x1-x3)+(y1-y3)*(y1-y3));
+	if (a==0 || b==0 || c==0) {
+		return false;
+	}
 	if (a>b) {
 		if (b==c && a==Math.sqrt(b*b+c*c)) {
 			return true;
