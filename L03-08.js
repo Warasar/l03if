59053e059693f96ed5631ac05370049e6f837078
uufswe21 <sdfauren@gmail.com>
2018-10-22 20:11:22 +0900

    -8

--- L03-08.js
+++ L03-08.js
@@ -7,6 +7,7 @@ function S(x1,y1,x2,y2,x3,y3) {
 	var L12 = Math.sqrt(x12*x12+y12*y13);
 	var L13 = Math.sqrt(x13*x13+y13*y13);
 	var cos213 = sk1213/(L12*L13);
+	if (cos213 <= 0) return -1;
 	
 	var x21 = x1 - x2;
 	var y21 = y1 - y2;
@@ -16,6 +17,7 @@ function S(x1,y1,x2,y2,x3,y3) {
 	var L21 = Math.sqrt(x21*x21+y21*y21);
 	var L23 = Math.sqrt(x23*x23+y23*y23);
 	var cos123 = sk2123/(L21*L23);
+	if (cos123 <= 0) return -1;
 	
 	var x31 = x1 - x3;
 	var y31 = y1 - y3;
@@ -25,11 +27,13 @@ function S(x1,y1,x2,y2,x3,y3) {
 	var L31 = Math.sqrt(x31*x31+y31*y31);
 	var L32 = Math.sqrt(x32*x32+y32*y32);
 	var cos132 = sk3132/(L31*L32);
+	if (cos132 <= 0) return -1;
 	
 	var k = 0;
 	
-	if (Math.abs(cos213) < Math.PI && Math.abs(cos123) < Math.PI && Math.abs(cos132) < Math.PI) k = 1;
-		else
-			k = -1;
-	
+	if (Math.abs(cos213) < Math.PI && Math.abs(cos123) < Math.PI && Math.abs(cos132) < Math.PI) {
+		var p = (L12+L13+L23)/2;
+		var ss = Math.sqrt(p*(p-L12)*(p-L13)*(p-L23));
+		return ss;
+	}
 }
