db97e9a44256bde4c1e2ac1e8ad2514da4bb86ad
gavrilmikhailov <gavrilmikhailov@gmail.com>
2018-10-22 22:35:19 +0900

    12 and 14

--- L03-12.js
+++ L03-12.js
@@ -1,3 +1,39 @@
 // 12.	(Использовать switch/case. Массивы, операторы if не использовать)
 // 		Введите с клавиатуры число n и выведите английское название соответствующей цифры
 // 		от 0 до 9 заглавными буквами, (ZERO, ONE, ...), либо ERROR, если число меньше 0 или больше 9.
+function digit(n) {
+switch(n) {
+	case 0:
+		return 'ZERO';
+		break;
+	case 1:
+		return 'ONE';
+		break;
+	case 2:
+		return 'TWO';
+		break;
+	case 3:
+		return 'THREE';
+		break;
+	case 4:
+		return 'FOUR';
+		break;
+	case 5:
+		return 'FIVE';
+		break;
+	case 6:
+		return 'SIX';
+		break;
+	case 7:
+		return 'SEVEN';
+		break;
+	case 8:
+		return 'EIGHT';
+		break;
+	case 9:
+		return 'NINE';
+		break;
+	default:
+		return 'ERROR';
+	}
+}
