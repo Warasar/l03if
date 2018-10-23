46744be8b5a48c8aced9a0dd438bdb09d58209e7
y2k01 <43157835+y2k01@users.noreply.github.com>
2018-10-23 10:10:42 +0900

    Update and rename L03-x3.js to L03-10.js

---
+++ L03-10.js
@@ -0,0 +1,29 @@
+function MonthName(n){
+ switch(n){
+    case 1: alert("Январь") 
+    break
+    case 2: alert("Февраль") 
+    break
+    case 3: alert("Март") 
+    break
+    case 4: alert("Апрель")
+    break
+    case 5: alert("Май") 
+    break
+    case 6: alert("Июнь") 
+    break
+    case 7: alert("Июль") 
+    break
+    case 8: alert("Август") 
+    break
+    case 9: alert("Сентьябрь")
+    break
+    case 10: alert("Октябрь")
+    break
+    case 11: alert("Ноябрь")
+    break
+    case 12: alert("Декабрь")
+    break
+    default: alert("Нет такого месяца")
+ }
+}
