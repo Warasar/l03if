124f78daeabad198c8529e0ca02835b627920d1c
timofeeva358456 <43157821+timofeeva358456@users.noreply.github.com>
2018-10-23 10:07:14 +0900

    Update L03-05.js

--- L03-05.js
+++ L03-05.js
@@ -1,8 +1,15 @@
-function h (a, b,c){
-	var a, b, c, res;
-	if (a + b > c and a + c > b and b + c > a)
-		write ("");
-	else 
-		write ("1");
-	
-}
+function h(a,b,c)
+{
+	var s,p,res;
+	if (a+b>=c)
+	{
+		if (a+c>=b)
+		{
+			if (c+b>=a)
+			{
+				s=(a+b+c)/2;
+				res=(2*s)/a;
+			}else res=-1;
+		}else res=-1;
+	}else res=-1;
+	return(res);
