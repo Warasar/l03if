eb3919ceecc6aa26ce346948dea6b8707a2e61ae
gavrilmikhailov <gavrilmikhailov@gmail.com>
2018-10-29 22:07:34 +0900

    i

--- L03-07.js
+++ L03-07.js
@@ -1,4 +1,4 @@
-function S(x1,y1,x2,y2,x3,y3) {
+ function S(x1,y1,x2,y2,x3,y3) {
 	var b1,b2,b3,p,res;
 	b1 = Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2));
 	b2 = Math.sqrt(Math.pow((x3-x2),2)+Math.pow((y3-y2),2));
@@ -8,4 +8,4 @@ function S(x1,y1,x2,y2,x3,y3) {
 		res = Math.sqrt(p*(p-b1)*(p-b2)*(p-b3));
 	else res = -1;
 	return res;
-}
+}	
