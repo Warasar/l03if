97c62394e3cc63e56f4a79e3621549d30a5f8952
petrovm <petrovm@gmail.com>
2018-10-30 10:14:08 +0900

    '14'

---
+++ L03-14.js
@@ -0,0 +1,29 @@
+function skal(x1,y1,x2,y2,x3,y3) 
+{
+	var x12=x2-x1;
+	var y12=y2-y1;
+	var x13=x3-x1;
+	var y13=y3-y1;
+	var sk1213=x12*x13+y12*y13;
+	return sk1213;
+}
+
+function x4(x1,y1,x2,y2,x3,y3) 
+{
+	if (Math.abs(skal(x1,y1,x2,y2,x3,y3))<0.0001)
+		return (x3+(x2-x1));
+	if (Math.abs(skal(x2,y2,x1,y1,x3,y3))<0.0001)
+		return (x3+(x1-x2));
+	if (Math.abs(skal(x3,y2,x1,y1,x2,y2))<0.0001)
+		return (x2+(x1-x3));
+}
+
+function Y4(x1,y1,x2,y2,x3,y3) 
+{
+	if (Math.abs(skal(x1,y1,x2,y2,x3,y3))<0.0001)
+		return (y3+(y2-y1));
+	if (Math.abs(skal(x2,y2,x1,y1,x3,y3))<0.0001)
+		return (y3+(y1-y2));
+	if (Math.abs(skal(x3,y3,x1,y1,x2,y2))<0.0001)
+		return (y2+(y1-y3));
+}
