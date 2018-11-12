815f5d927dd749d0bbf72676b6b586ea4c687e35
timofeeva358456 <43157821+timofeeva358456@users.noreply.github.com>
2018-11-13 06:00:00 +0900

    Update L03-11.js

---
+++ L03-11.js
@@ -0,0 +1,29 @@
+function WeekdayName(n) {
+	var week;
+	switch (n) {
+		case 1:
+		    week = 'понедельник';
+		    break;
+		case 2:
+		    week = 'вторник';
+		    break;
+		case 3:
+		    week = 'среда';
+		    break;
+		case 4:
+		    week = 'четверг';
+		    break;
+		case 5:
+		    week = 'пятница';
+		    break;
+		case 6:
+		    week = 'суббота';
+		    break;
+		case 7:
+		    week = 'воскресенье';
+		    break;
+		default:
+			week = '';
+	}
+	return week;
+}
